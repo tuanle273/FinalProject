fe17e0440a3794c46e80a5256a4dfdf2a1c0571f
tuanle273 <tuanle273@gmail.com>
2022-12-20 01:28:56 +0700

    done connect to database Mongodb

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const authRouter = require("./routes/auth");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const db = mongoose.connection;
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+
+dotenv.config();
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected!"));
+db.on("error", (err) => {
+  console.log("DB connection error:", err.message);
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on http://localhost:" + PORT);
+});
+
+module.exports = app;
