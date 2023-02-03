77928e778b06027f1f70a7957f3066837df5e0ed
tuanle273 <tuanle273@gmail.com>
2023-02-03 16:05:10 +0700

    Login success and Redirect to Home. Token in LS

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import { Redirect } from "react-router-dom";
 import LoginForm from "../components/auth/LoginForm";
 import RegisterForm from "../components/auth/RegisterForm";
+import { AuthContext } from "../contexts/AuthContext";
+
 const Auth = ({ authRoute }) => {
+  const {
+    authState: { authLoading, isAuthenticated },
+  } = useContext(AuthContext);
+
   let body;
 
-  body = (
-    <>
-      {authRoute === "login" && <LoginForm />}
-      {authRoute === "register" && <RegisterForm />}
-    </>
-  );
+  if (authLoading)
+    body = (
+      <div className="d-flex justify-content-center mt-2">
+        <Spinner animation="border" variant="info" />
+      </div>
+    );
+  else if (isAuthenticated) return <Redirect to="/home" />;
+  else
+    body = (
+      <>
+        {authRoute === "login" && <LoginForm />}
+        {authRoute === "register" && <RegisterForm />}
+      </>
+    );
   return (
     <div className="landing">
       <div className="dark-overlay">
