6108cc6c1d6778f1f29f2fb7fc7ec909e67702aa
Anh Tuan <tuanle273@gmail.com>
2023-02-08 01:23:30 +0700

    done get userDetail

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 const User = require("../models/User");
+
 const getUser = async (req, res) => {
   try {
-    const userDetail = await User.find({ user: req.userId });
+    const userDetail = await User.findById(req.userId);
     res.json({ success: true, userDetail });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 module.exports = {
