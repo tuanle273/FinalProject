285ff2b41c700a933f6d6937115a4c0187166e6d
Anh Tuan <tuanle273@gmail.com>
2023-02-10 13:03:02 +0700

    login by google #2

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,10 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const verifyToken = require("../middlewares/auth");
 
+const app = express();
 const userController = require("../controllers/userController");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 // @route GET api/auth/getUser
 // @des Check user details
 //@access public
@@ -19,4 +22,71 @@ router.get("/admin", userController.authenticateRole(["admin"]), (req, res) => {
 router.get("/owner", userController.authenticateRole(["owner"]), (req, res) => {
   res.send({ success: "Owner zone." });
 });
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./../utils/key");
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "http://localhost:5000/api/user/auth/google/callback",
+    },
+    (profile, done) => {
+      // Check if google profile exist.
+      if (profile.id) {
+        User.findOne({ googleId: profile.id }).then((existingUser) => {
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            new User({
+              googleId: profile.id,
+              email: profile.emails[0].value,
+              name: profile.name.familyName + " " + profile.name.givenName,
+            })
+              .save()
+              .then((user) => done(null, user));
+          }
+        });
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define the authentication routes
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+app.get("/api/current_user", (req, res) => {
+  res.send(req.user);
+});
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:3000/login",
+  }),
+  function (req, res) {
+    // Successful authentication, redirect to the dashboard
+    res.redirect("/dashboard");
+  }
+);
 module.exports = router;
---
+++ utils/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "130369912087-k9dh5fm2cuim12mrokvrv8hrsa9ddt0c.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
