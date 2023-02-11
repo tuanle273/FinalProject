230c309e36f9190c4443bcbdfa44a9b3df74cf30
Anh Tuan <tuanle273@gmail.com>
2023-02-11 18:58:52 +0700

    fix login google #4

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,54 +1,19 @@
-import { useContext, useEffect } from "react";
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import { AuthContext } from "../../contexts/AuthContext";
-import { PostContext } from "../../contexts/PostContext";
-import SinglePost from "../posts/SinglePost";
 const HomePage = () => {
-  const {
-    authState: {
-      user: { username },
-    },
-  } = useContext(AuthContext);
-  const {
-    postState: { posts, postsLoading },
-    getPosts,
-  } = useContext(PostContext);
-
-  //Start get all post
-  useEffect(() => getPosts(), []);
-
-  let body = null;
-  if (posts.length === 0) {
-    body = (
-      <>
-        <Card className="text-center mx-5 my-5">
-          <Card.Header as="h1">Hi {username}</Card.Header>
-          <Card.Body>
-            <Card.Title>Welcome to ConnyCar</Card.Title>
-            <Card.Text>Click to track</Card.Text>
-            <Button variant="primary">Rent</Button>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  } else {
-    body = (
-      <>
-        <Row className="row-cols-1 row-cols-md-3 g-4 mx-auto mt-3">
-          {posts.map((post) => (
-            <Col key={post._id} className="my-2">
-              <SinglePost post={post}></SinglePost>
-            </Col>
-          ))}
-        </Row>
-      </>
-    );
-  }
-
-  return <>{body}</>;
+  return (
+    <div>
+      <Card className="text-center mx-4 my-2 bg-slate-50">
+        <Card.Header as="h1">Hi </Card.Header>
+        <Card.Body>
+          <Card.Title>Welcome to ConnyCar</Card.Title>
+          <Card.Text>Click to track</Card.Text>
+          <Button variant="primary">Rent</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default HomePage;
