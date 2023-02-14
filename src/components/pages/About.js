f8215c853deff212335a57be872584bdfcbbad0c
Anh Tuan <tuanle273@gmail.com>
2023-02-14 13:13:54 +0700

    login google #4

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,37 +1,9 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from 'react'
 
 const About = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/vehicle")
-      .then((res) => {
-        console.log(res);
-        setPosts(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
   return (
-    <div>
-      <h1>Vehicle</h1>
-      {posts ? (
-        <>
-          {posts.map((post) => {
-            return (
-              <div>
-                <h4>{posts.title}</h4>
-                <p>{posts.description}</p>
-              </div>
-            );
-          })}
-        </>
-      ) : (
-        ""
-      )}
-    </div>
-  );
-};
+    <div>About</div>
+  )
+}
 
-export default About;
+export default About
