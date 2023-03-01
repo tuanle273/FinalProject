6dc0858e21a478245624077c3aaca632fb1cc75e
tuanle273 <tuanle273@gmail.com>
2023-03-02 02:02:40 +0700

    updated user profile, edit , delete vehicle done

--- client/src/components/pages/Dashboard/Modal/DeleteModal.js
+++ client/src/components/pages/Dashboard/Modal/DeleteModal.js
@@ -1,10 +1,28 @@
-import React from "react";
-export default function DeleteModal(isModalOpen, handleOk, handleModalShow) {
-   const [showModal, setShowModal] = React.useState(false);
+import React, { useContext, useState } from "react";
+import Form from "react-bootstrap/Form";
+import { toast, Toaster } from "react-hot-toast";
+import { VehicleContext } from "../../../../contexts/VehicleContext";
+
+const DeleteModal = (props) => {
+  const { deleteVehicle } = useContext(VehicleContext);
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    const response = await deleteVehicle(props.itemId);
+    console.log("🚀 ~ file: DeleteModal.js:12 ~ handleDelete ~ response:", response)
+    if (response.success) {
+      toast.success(response.message);
+      props.handleClose();
+    } else {
+      toast.error(response.message);
+    }
+  };
   return (
+    <div>  
+      <Toaster/>
+      {" "}
     
-    <>
-      {showModal ? (
+      {props.show ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -12,10 +30,10 @@ export default function DeleteModal(isModalOpen, handleOk, handleModalShow) {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <h3 className="text-3xl font-semibold">Delete </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
+                    onClick={props.handleClose}
                   >
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
@@ -24,30 +42,27 @@ export default function DeleteModal(isModalOpen, handleOk, handleModalShow) {
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                   Are you sure?
                   </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={props.handleClose}
+                      >
+                        Close
+                      </button>
+                      <button
+                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="submit"
+                        onClick={handleDelete}
+                      >
+                        Save Changes
+                      </button>
+                    </div>
+              
                 </div>
               </div>
             </div>
@@ -55,6 +70,9 @@ export default function DeleteModal(isModalOpen, handleOk, handleModalShow) {
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
       ) : null}
-    </>
+      <Toaster />
+    </div>
   );
-}
+};
+
+export default DeleteModal;
