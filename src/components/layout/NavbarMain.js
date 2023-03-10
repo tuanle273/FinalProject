e601927db90830454dae9fd358cb918203326ca0
tuanle273 <tuanle273@gmail.com>
2023-03-11 00:25:37 +0700

    fix some bug

--- src/components/layout/NavbarMain.js
+++ src/components/layout/NavbarMain.js
@@ -1,7 +1,6 @@
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import React, { Fragment, useContext } from "react";
-import Marquee from "react-fast-marquee";
 import { Link, useLocation } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 const NavbarMain = () => {
@@ -101,16 +100,6 @@ const NavbarMain = () => {
 
   return (
     <div className="min-h-full">
-      <Marquee speed={100} gradient={false}>
-        <span className="bg-yellow-100 text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
-          News
-        </span>
-        Rent a car for your next trip! Get great deals on luxury and economy
-        cars at Crab Car Rental - Book now and save big!
-        <span className="bg-yellow-100 text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
-          News
-        </span>
-      </Marquee>
       <Disclosure as="nav" className="bg-gray-800">
         {({ open }) => (
           <>
--- src/components/pages/Logged/HomePage.js
+++ src/components/pages/Logged/HomePage.js
@@ -1,135 +1,182 @@
 import React, { Fragment, useContext, useEffect, useState } from "react";
-import { toast, Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { VehicleContext } from "../../../contexts/VehicleContext";
 
 import { Link } from "react-router-dom";
 export default function HomePage() {
   const [vehicles, setVehicle] = useState(null);
- 
-  const {
-    loadVehicles
-  } = useContext(VehicleContext);
+
+  const { loadVehicles } = useContext(VehicleContext);
   useEffect(() => {
     const loadVehicle = async () => {
       const response = await loadVehicles();
-     
+
       setVehicle(response.data);
     };
-    loadVehicle()}, []);
+    loadVehicle();
+  }, []);
 
-      return (
-        <Fragment>
-          <main>
-            {" "}
-            <Toaster />
-            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-              <div className="px-4 py-6 sm:px-0">
-                <div
-                  className="grid grid-cols-4 gap-4 "
-                  style={{ marginLeft: "5em" }}
-                >
-                  {vehicles &&
-                    vehicles.map((item) => (
-                      <div key={item._id}>
-                        <div className="bg-gray-800 w-60 shadow-lg rounded p-2">
-                          <div className="py-2 px-4 text-center tracking-wide grid grid-cols-3 gap-6">
-                            <div className="flex tools">
-                              <p className="flex text-gray-400 text-sm justify-center">
-                                Seat
-                              </p>
-                              <p className="text-sm text-gray-50 animate-pulse px-2">
-                                {item.seat}
-                              </p>
-                            </div>
-                            <div className="flex followers">
-                              <p className="flex text-gray-400 text-sm justify-center">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="h-5 w-5"
-                                  viewBox="0 0 24 24"
-                                  stroke="currentColor"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                                  />
-                                </svg>
-                              </p>
-                              <p className="text-sm text-gray-50 animate-pulse px-2">
-                                {" "}
-                                {item.capacity}
-                              </p>
-                            </div>
-                            <div className="flex following">
-                              <p className="flex text-gray-400 text-sm justify-center">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="h-5 w-5"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke="currentColor"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-                                  />
-                                </svg>
-                              </p>
-                              <p className="text-sm text-gray-50 animate-pulse px-2">
-                                {" "}
-                                {item.transmission}
-                              </p>
-                            </div>
-                          </div>
-                          <div className="group relative">
-                            <img
-                              alt="Placeholder"
-                              className="block h-48 w-full rounded"
-                              src={item.imageUrl}
-                            />
-                          </div>
-                          <div className="p-2">
-                            <h3 className=" text-white py-1 text-base justify-center">
-                              {item.title.toUpperCase()}
-                            </h3>
-                            <p className="text-gray-400 text-sm">
-                              Model: {item.model}
-                            </p>
-                            <p className="text-gray-400 text-sm">
-                              Price: {item.price} $
-                            </p>
-                            <p className="text-gray-400 text-sm">
-                              Available: {String(item.availability)}
-                            </p>
-                            <span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                              #{item.type}
-                            </span>
-                          </div>
-                          <div className="flex items-center flex-auto justify-between">
-                            <a
-                              href="#"
-                              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  return (
+    <Fragment>
+      <main>
+        {" "}
+        <Toaster />
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-6 sm:px-0">
+            <div
+              className="grid grid-cols-4 gap-4 "
+              style={{ marginLeft: "5em" }}
+            >
+              {vehicles &&
+                vehicles.map((item) => (
+                  <div key={item._id}>
+                    <div className="bg-gray-800 w-60 shadow-lg rounded p-2">
+                      <div className="py-2 px-4 text-center tracking-wide grid grid-cols-3 gap-6">
+                        <div className="flex tools">
+                          <p className="flex text-gray-400 text-sm justify-center">
+                            Seat
+                          </p>
+                          <p className="text-sm text-gray-50 animate-pulse px-2">
+                            {item.seat}
+                          </p>
+                        </div>
+                        <div className="flex followers">
+                          <p className="flex text-gray-400 text-sm justify-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-5 w-5"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
                             >
-                              Book
-                            </a>
-                            <Link  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" to={`/vehicleDetail/${item._id}`}>
-                              View Detail
-                         </Link>
-                          </div>
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                              />
+                            </svg>
+                          </p>
+                          <p className="text-sm text-gray-50 animate-pulse px-2">
+                            {" "}
+                            {item.capacity}
+                          </p>
+                        </div>
+                        <div className="flex following">
+                          <p className="flex text-gray-400 text-sm justify-center">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-5 w-5"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                              />
+                            </svg>
+                          </p>
+                          <p className="text-sm text-gray-50 animate-pulse px-2">
+                            {" "}
+                            {item.transmission}
+                          </p>
                         </div>
                       </div>
-                    ))}
-                </div>
-              </div>
+                      <div className="group relative">
+                        <img
+                          alt="Placeholder"
+                          className="block h-48 w-full rounded"
+                          src={item.imageUrl}
+                        />
+                      </div>{" "}
+                      <p className="text-gray-400 text-sm inline-block ">
+                        <div class="flex items-center">
+                          <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-400"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>First star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-400"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Second star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-400"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Third star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-yellow-400"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Fourth star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-gray-300 dark:text-gray-500"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Fifth star</title>
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                        </div>
+                      </p>
+                      <div className="p-2">
+                        <h3 className=" text-white py-1 text-base justify-center">
+                          {item.title.toUpperCase()}
+                        </h3>
+                        <p className="text-gray-400 text-sm">
+                          Model: {item.model}
+                        </p>
+                        <p className="text-gray-400 text-sm">
+                          Price: {item.price} $
+                        </p>
+                        <p className="text-gray-400 text-sm">
+                          Available: {String(item.availability)}
+                        </p>
+                        <span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                          #{item.type}
+                        </span>
+                      </div>
+                      <div className="flex items-center flex-auto justify-between">
+                        <Link
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          to={`/vehicleDetail/${item._id}`}
+                        >
+                          View Detail
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ))}
             </div>
-          </main>
-        </Fragment>
-      );
-  
-  }
-  
-
+          </div>
+        </div>
+      </main>
+    </Fragment>
+  );
+}
