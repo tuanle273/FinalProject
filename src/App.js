7532102e12a53016575ace7170190152aff7dc38
tuanle273 <tuanle273@gmail.com>
2023-04-08 07:53:28 +0700

    fix payment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Landing from "./components/layout/Landing";
 import NotFound from "./components/pages/404/NotFound";
@@ -7,7 +8,6 @@ import BookingManagement from "./components/pages/Dashboard/BookingManagement";
 import BrandManagement from "./components/pages/Dashboard/BrandManagement";
 import Chart from "./components/pages/Dashboard/Chart";
 import DiscountManagement from "./components/pages/Dashboard/DiscountManagement";
-
 import UserManagement from "./components/pages/Dashboard/UserManagement";
 import VehicleManagement from "./components/pages/Dashboard/VehicleManagement";
 import ForgotPassword from "./components/pages/ForgotPassword";
@@ -22,112 +22,144 @@ import PasswordReset from "./components/pages/PasswordReset";
 import AdminRoute from "./components/routing/AdminRoute";
 import ProtectedRoute from "./components/routing/ProtectedRoute";
 import AuthContextProvider from "./contexts/AuthContext";
+import { BookingProvider } from "./contexts/BookingContext";
+import { BrandProvider } from "./contexts/BrandContext";
+import { ChartProvider } from "./contexts/ChartContext";
+import { DiscountProvider } from "./contexts/DiscountContext";
+import { UserProvider } from "./contexts/UserContext";
+import { VehicleProvider } from "./contexts/VehicleContext";
 import Auth from "./views/Auth";
 
 function App() {
   return (
     <AuthContextProvider>
-      <Router>
-        {" "}
-        <Switch>
-          {" "}
-          <ProtectedRoute
-            exact
-            path="/profile"
-            component={UserProfile}
-          ></ProtectedRoute>{" "}
-          <ProtectedRoute
-            exact
-            path="/history"
-            component={History}
-          ></ProtectedRoute>
-          <ProtectedRoute
-            exact
-            path="/about"
-            component={About}
-          ></ProtectedRoute>
-          <ProtectedRoute
-            exact
-            path="/vehicleDetail/:vehicleId"
-            component={vehicleDetail}
-          ></ProtectedRoute>
-          <ProtectedRoute
-            exact
-            path="/checkout/:vehicleId"
-            component={CheckOut}
-          ></ProtectedRoute>
-          <Route
-            exact
-            path="/forgotpassword"
-            component={ForgotPassword}
-          ></Route>
-          <Route
-            exact
-            path="/passwordreset/:token"
-            component={PasswordReset}
-          ></Route>
-          <AdminRoute
-            exact
-            path="/admin"
-            component={UserManagement}
-          ></AdminRoute>
-          <AdminRoute
-            exact
-            path="/admin/general"
-            component={Chart}
-          ></AdminRoute>
-          <AdminRoute
-            exact
-            path="/admin/usermanagement"
-            component={UserManagement}
-          ></AdminRoute>
-          <AdminRoute
-            exact
-            path="/admin/vehiclemanagement"
-            component={VehicleManagement}
-          ></AdminRoute>
-          <AdminRoute
-            exact
-            path="/admin/brandmanagement"
-            component={BrandManagement}
-          ></AdminRoute>
-          <AdminRoute
-            exact
-            path="/admin/bookingmanagement"
-            component={BookingManagement}
-          ></AdminRoute>{" "}
-          <AdminRoute
-            exact
-            path="/admin/discountmanagement"
-            component={DiscountManagement}
-          ></AdminRoute>
-          <ProtectedRoute
-            exact
-            path="/checkout"
-            component={CheckOut}
-          ></ProtectedRoute>
-          <ProtectedRoute exact path="/faq" component={Faq}></ProtectedRoute>{" "}
-          <ProtectedRoute
-            exact
-            path="/allcar"
-            component={AllCars}
-          ></ProtectedRoute>
-          <ProtectedRoute exact path="/" component={Landing}></ProtectedRoute>
-          <Route
-            exact
-            path="/login"
-            render={(props) => <Auth {...props} authRoute="login" />}
-          ></Route>
-          <Route
-            exact
-            path="/register"
-            render={(props) => <Auth {...props} authRoute="register" />}
-          ></Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+      <VehicleProvider>
+        <BookingProvider>
+          <UserProvider>
+            <DiscountProvider>
+              <BrandProvider>
+                <ChartProvider>
+                  <ProSidebarProvider>
+                    <Router>
+                      {" "}
+                      <Switch>
+                        {" "}
+                        <ProtectedRoute
+                          exact
+                          path="/profile"
+                          component={UserProfile}
+                        ></ProtectedRoute>{" "}
+                        <ProtectedRoute
+                          exact
+                          path="/history"
+                          component={History}
+                        ></ProtectedRoute>
+                        <ProtectedRoute
+                          exact
+                          path="/about"
+                          component={About}
+                        ></ProtectedRoute>
+                        <ProtectedRoute
+                          exact
+                          path="/vehicleDetail/:vehicleId"
+                          component={vehicleDetail}
+                        ></ProtectedRoute>
+                        <ProtectedRoute
+                          exact
+                          path="/checkout/:vehicleId"
+                          component={CheckOut}
+                        ></ProtectedRoute>
+                        <Route
+                          exact
+                          path="/forgotpassword"
+                          component={ForgotPassword}
+                        ></Route>
+                        <Route
+                          exact
+                          path="/passwordreset/:token"
+                          component={PasswordReset}
+                        ></Route>
+                        <AdminRoute
+                          exact
+                          path="/admin"
+                          component={UserManagement}
+                        ></AdminRoute>
+                        <AdminRoute
+                          exact
+                          path="/admin/general"
+                          component={Chart}
+                        ></AdminRoute>
+                        <AdminRoute
+                          exact
+                          path="/admin/usermanagement"
+                          component={UserManagement}
+                        ></AdminRoute>
+                        <AdminRoute
+                          exact
+                          path="/admin/vehiclemanagement"
+                          component={VehicleManagement}
+                        ></AdminRoute>
+                        <AdminRoute
+                          exact
+                          path="/admin/brandmanagement"
+                          component={BrandManagement}
+                        ></AdminRoute>
+                        <AdminRoute
+                          exact
+                          path="/admin/bookingmanagement"
+                          component={BookingManagement}
+                        ></AdminRoute>{" "}
+                        <AdminRoute
+                          exact
+                          path="/admin/discountmanagement"
+                          component={DiscountManagement}
+                        ></AdminRoute>
+                        <ProtectedRoute
+                          exact
+                          path="/checkout"
+                          component={CheckOut}
+                        ></ProtectedRoute>
+                        <ProtectedRoute
+                          exact
+                          path="/faq"
+                          component={Faq}
+                        ></ProtectedRoute>{" "}
+                        <ProtectedRoute
+                          exact
+                          path="/allcar"
+                          component={AllCars}
+                        ></ProtectedRoute>
+                        <ProtectedRoute
+                          exact
+                          path="/"
+                          component={Landing}
+                        ></ProtectedRoute>
+                        <Route
+                          exact
+                          path="/login"
+                          render={(props) => (
+                            <Auth {...props} authRoute="login" />
+                          )}
+                        ></Route>
+                        <Route
+                          exact
+                          path="/register"
+                          render={(props) => (
+                            <Auth {...props} authRoute="register" />
+                          )}
+                        ></Route>
+                        <Route path="*">
+                          <NotFound />
+                        </Route>
+                      </Switch>
+                    </Router>
+                  </ProSidebarProvider>
+                </ChartProvider>
+              </BrandProvider>
+            </DiscountProvider>
+          </UserProvider>
+        </BookingProvider>{" "}
+      </VehicleProvider>
     </AuthContextProvider>
   );
 }
