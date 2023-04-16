90358dbf171d58c426087597409e6ea17ec3dd6b
tuanle273 <tuanle273@gmail.com>
2023-04-17 01:15:03 +0700

    fix lib

--- server/controllers/vehicleController.js
+++ server/controllers/vehicleController.js
@@ -1,10 +1,4 @@
-const express = require("express");
-const { response } = require("../index");
-const router = express.Router();
-const verifyToken = require("../middlewares/auth");
 const Vehicle = require("../models/Vehicle");
-const VehicleController = require("./VehicleController");
-const Brand = require("../models/Brand");
 
 const getVehicle = async (req, res) => {
   try {
