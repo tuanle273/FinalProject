a7051393f4d79242244ed172616a8f84f1304fd0
tuanle273 <tuanle273@gmail.com>
2023-04-17 01:39:10 +0700

    update URL database for netlify

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
+    ? "https://tt-wdv1.onrender.com/api"
     : "somedeployURL";
 export const LOCAL_STORAGE_TOKEN_NAME = "connycar";
 export const SET_AUTH = "SET_AUTH";
