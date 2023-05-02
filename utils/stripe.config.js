1d9178bad34203dce873eda425bece42c70e3b1f
Anh Tuan <tuanle273@gmail.com>
2023-05-02 14:00:09 +0700

    edit footer

--- utils/stripe.config.js
+++ utils/stripe.config.js
@@ -1,19 +1,36 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const createPayment = async (req, res) => {
-  const { amount } = req.body;
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: "price_1MtwkwAIZ2hFgEmPwyKD5Vgp",
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `http://localhost:3000/login`,
-    cancel_url: `http://localhost:3000/login`,
-  });
+  const { amount, description, imgUrl } = req.query;
 
-  res.redirect(303, session.url);
+  try {
+    // Create a price with the given amount and description
+    const price = await stripe.prices.create({
+      unit_amount: amount, // the price amount in cents
+      currency: "usd", // the currency of the price
+      product_data: {
+        name: description,
+      },
+    });
+
+    // Create a checkout session with the created price
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: price.id,
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `http://localhost:3000/login`,
+      cancel_url: `http://localhost:3000/login`,
+    });
+
+    // Redirect the user to the checkout page
+    res.redirect(303, session.url);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating payment session" });
+  }
 };
 const getAllTransactions = async (req, res) => {
   try {
