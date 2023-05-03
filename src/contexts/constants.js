63b35a21be3a1bf8a2e78d3efde90ccbb7faf885
Anh Tuan Le <63275793+tuanle273@users.noreply.github.com>
2023-05-03 07:54:31 -0700

    Update constants.js

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "https://tt-wdv1.onrender.com/api";
+    : "https://server2-vxcz.onrender.com/api";
 export const LOCAL_STORAGE_TOKEN_NAME = "connycar";
 export const SET_AUTH = "SET_AUTH";
 export const UPDATE_USER_PROFILE = "UPDATE_USER_PROFILE";
